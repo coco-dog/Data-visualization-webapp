1109563eb9142e03158daf0ad802559297e7adf5
吴正卓 <1993441338@qq.com>
2021-10-09 19:19:30 +0800

    添加knife4j组件

--- wu-school-adminweb-app/new-crown-outbreak/src/main/java/com/wu/xxljobdemo/controller/LocalController.java
+++ wu-school-adminweb-app/new-crown-outbreak/src/main/java/com/wu/xxljobdemo/controller/LocalController.java
@@ -7,6 +7,8 @@ import com.wu.xxljobdemo.vo.CountyVO;
 import com.wu.xxljobdemo.vo.LineVO;
 import com.wu.xxljobdemo.vo.StateVO;
 import com.wu.xxljobdemo.vo.StateandCases;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.Set;
 
+@Api(tags = "疫情接口文档")
 @RestController
 @RequestMapping("//local")
 public class LocalController {
@@ -24,42 +27,51 @@ public class LocalController {
     @Autowired
     private LocalService localService;
 
-    /*累积感染死亡和病死率*/
+
+    @ApiOperation(value = "累积感染死亡和病死率")
     @GetMapping("/total")
     public Local total(){return localService.total();}
 
-    /*全国地图数据*/
+
+    @ApiOperation(value = "全国地图数据")
     @GetMapping("/statemap")
     public StateandCases geo(){
         return localService.geo();
     }
 
-    /*病死率折线图*/
+
+    @ApiOperation(value = "病死率折线图")
     @GetMapping("/line")
     public LineVO line(){
         return localService.cfr();
     }
-    /*右侧搜索框数据*/
+
+
+    @ApiOperation(value = "搜索框报表区数据")
     @GetMapping("/countymap")
     public CountyVO state(String state){ return localService.search(state); }
 
-    /*区数据,感染死亡感染率*/
+
+    @ApiOperation(value = "区数据,感染死亡率")
     @GetMapping("/countydate")
     public List<Local> county(){return localService.excel();}
 
     /*搜索框中州的数据*/
+    @ApiOperation(value = "报表中州的数据")
     @GetMapping("/searchstateline")
     public LineVO searchstateline(String state){
         return localService.searchstateline(state);
     }
 
-    /*某个州中各区的数据*/
+
+    @ApiOperation(value = "某个州中各区的数据")
     @GetMapping("/detaildata")
     public List<Local> detaildata(String state){
         return localService.detaildata(state);
     }
 
-    /*所有州名*/
+
+    @ApiOperation(value = "所有州名")
     @GetMapping("/allstate")
     public Set<String> allstate(){
         return (Set<String>)redisTemplate.opsForSet().members("allstate");
